d47a2e97b2aae6e1595549ab81dae21ef6b4e4cd
curriecode <melaniecurrie01@gmail.com>
2020-02-28 11:56:28 -0800

    added: cdn and scope for commerce.js sdk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import React from "react";
+import Commerce from "@chec/commerce.js";
+const commerce = new Commerce("{public_api_key}", true);
 
 function App() {
   return <button>ADD PRODUCT</button>;
