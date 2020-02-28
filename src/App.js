fa733421214202a8ebadcbb5ab08ff0f97321004
curriecode <melaniecurrie01@gmail.com>
2020-02-28 13:40:20 -0800

    added: product cards / styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useState } from "react";
 import Commerce from "@chec/commerce.js";
+import Product from "./Product";
 const commerce = new Commerce(
   "pk_1784477b7bd2dc0841e1125b4bb84674c27de3cc87938",
   true
@@ -9,19 +10,21 @@ const commerce = new Commerce(
 
 export default function App() {
   let [prodName, setProdName] = useState("");
+  let [description, setDescription] = useState("");
+  let [price, setPrice] = useState("");
+  let [photo, setPhoto] = useState("");
+
   function apichec() {
     commerce.products
       .list()
       .then(result => {
         console.log(result.data);
-        let names = result.data.map(
-          product =>
-            product.name +
-            product.description +
-            product.price.formatted_with_symbol +
-            product.media.source
-        );
-        setProdName(names);
+        result.data.map(product => {
+          setProdName(product.name);
+          setDescription(product.description);
+          setPrice(product.price.formatted_with_symbol);
+          setPhoto(product.media.source);
+        });
       })
       .catch(err => {
         console.log("ERROR", err);
@@ -30,7 +33,12 @@ export default function App() {
   return (
     <div>
       <button onClick={apichec}>ADD PRODUCT</button>
-      {prodName}
+      <Product
+        name={prodName}
+        description={description}
+        price={price}
+        photo={photo}
+      />
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,1 +1,15 @@
 import React from "react";
+import "./Product.css";
+
+export default function(props) {
+  // console.log("inside prodict", props.name);
+
+  return (
+    <div className="card">
+      <h1>{props.name}</h1>
+      <h2>{props.description}</h2>
+      <h3>{props.price}</h3>
+      <img className="pic" src={props.photo} />
+    </div>
+  );
+}
