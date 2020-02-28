dafdd4149348ec5043f15fc34400dbe6d442b78e
curriecode <melaniecurrie01@gmail.com>
2020-02-28 13:59:23 -0800

    updtae: now looping over cards to create items dynamically

--- src/Product.js
+++ src/Product.js
@@ -3,13 +3,16 @@ import "./Product.css";
 
 export default function(props) {
   // console.log("inside prodict", props.name);
-
-  return (
-    <div className="card">
-      <h1>{props.name}</h1>
-      <h2>{props.description}</h2>
-      <h3>{props.price}</h3>
-      <img className="pic" src={props.photo} />
-    </div>
-  );
+  const cards = props.prodInfo.map(product => {
+    let key = product.id;
+    return (
+      <div className="card" key={key}>
+        <h1>{product.name}</h1>
+        <h2>{product.description}</h2>
+        <h3>{product.price.formatted_with_symbol}</h3>
+        <img className="pic" src={product.media.source} />
+      </div>
+    );
+  });
+  return <div>{cards}</div>;
 }
