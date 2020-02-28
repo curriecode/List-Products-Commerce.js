ef4198ed53187c49faf91eb53e691d005e756706
curriecode <melaniecurrie01@gmail.com>
2020-02-28 13:01:24 -0800

    added: new component for prodict card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import "./App.css";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import Commerce from "@chec/commerce.js";
-const commerce = new Commerce("process.env.REACT_APP_CHEC_API_KEY", true);
-
-function apichec() {
-  axios
-    .get(commerce)
-    .then(res => {
-      console.log("SUCCESS", res);
-    })
-    .catch(err => {
-      console.log("ERROR", err);
-    });
-}
+const commerce = new Commerce(
+  "pk_1784477b7bd2dc0841e1125b4bb84674c27de3cc87938",
+  true
+);
 
 export default function App() {
-  return <button onClick={apichec}>ADD PRODUCT</button>;
+  let [prodName, setProdName] = useState("");
+  function apichec() {
+    commerce.products
+      .list()
+      .then(result => {
+        console.log(result.data);
+        let names = result.data.map(
+          product =>
+            product.name +
+            product.description +
+            product.price.formatted_with_symbol +
+            product.media.source
+        );
+        setProdName(names);
+      })
+      .catch(err => {
+        console.log("ERROR", err);
+      });
+  }
+  return (
+    <div>
+      <button onClick={apichec}>ADD PRODUCT</button>
+      {prodName}
+    </div>
+  );
 }
