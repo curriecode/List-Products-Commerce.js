2573a048c9d30f3f1906d37491284d1966c65f3c
curriecode <melaniecurrie01@gmail.com>
2020-03-02 11:32:18 -0800

    update: added logic to remove p tags from descrption

--- src/Product.js
+++ src/Product.js
@@ -4,13 +4,17 @@ import "./Product.css";
 export default function(props) {
   //Dynamically renders product cards based on response from Chec API
   const cards = props.prodInfo.map(product => {
+    //removes <p> from beginning of description string
+    let formatDescription = product.description.slice(3);
+    //removes </p> from end of description string
+    formatDescription = formatDescription.slice(0, -4);
     let key = product.id;
     return (
       <div className="card" key={key}>
         <h1>{product.name}</h1>
-        <h2>{product.description}</h2>
+        <h2>{formatDescription}</h2>
         <h3>{product.price.formatted_with_symbol}</h3>
-        <img className="pic" src={product.media.source} />
+        <img className="pic" src={product.media.source} alt="" />
       </div>
     );
   });
